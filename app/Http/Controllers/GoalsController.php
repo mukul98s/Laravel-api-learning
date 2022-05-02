1faa9be2562ee69d95f0c2821f78932fcfabc4c6
Mukul Sharma <mymukul112@gmail.com>
2022-05-02 11:24:12 +0530

    Add model route binding

--- app/Http/Controllers/GoalsController.php
+++ app/Http/Controllers/GoalsController.php
@@ -13,26 +13,31 @@ class GoalsController extends Controller
     }
 
     // get a single goals with @param $id
-    public function show($id) {
-        return Goals::find($id);
+    public function show(Goals $goal) {
+        return $goal;
     }
 
     // create a goal
     public function store(Request $request) {
-        return Goals::create($request->all());
+        $goal = Goals::create($request->all());
+
+        return response()->json($goal, 201);
     }
 
     // update a goal using id
     public function update(Request $request, $id){
         $goal = Goals::findOrFail($id);
         $goal->update($request->all());
-        return $goal;
+        return response()->json($goal, 200);
     }
 
     // delete a goal using id
     public function delete(Request $request, $id) {
         $goal = Goals::findOrFail($id);
         $goal->delete();
-        return $goal;
+        return response()->json([
+            'goal' => $goal,
+            'message' => 'Goal Deleted'
+        ], 200);
     }
 }
